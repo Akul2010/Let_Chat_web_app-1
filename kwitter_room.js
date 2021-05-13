f0a33c85c64d688780e3e7cfcaeb4c1e34eb4132
Akul2010 <75178259+Akul2010@users.noreply.github.com>
2021-05-13 16:56:54 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
